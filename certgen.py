c12e6977c0fb58211ad7532407bbb5c7994b13c4
James Brown <james.brown@r3.com>
2019-03-15 06:20:35 +0000

    adding verbose mode

--- certgen.py
+++ certgen.py
@@ -31,13 +31,25 @@ CERT_ROLES = {
 def cert_role_string(cr):
     return f'1.3.6.1.4.1.50530.1.1:non-critical={CERT_ROLES[cr]}'
 
-def execute(cmd):  
+def execute_verbose(cmd):  
   print(Fore.CYAN + cmd, flush=True)
   print(Style.RESET_ALL)
 
-  result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)#stdout=subprocess.PIPE)
+  try:
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)#stdout=subprocess.PIPE)
+  except:
+    pass
   print(result.stdout.decode('utf-8'))
   print(result.stderr.decode('utf-8'))    
+
+
+def execute(cmd):  
+  try:
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)#stdout=subprocess.PIPE)
+  except:
+    pass
+  #print(result.stdout.decode('utf-8'))
+  #print(result.stderr.decode('utf-8'))    
 
 class CertGenerator:
 
@@ -47,9 +59,9 @@ class CertGenerator:
 
   def __mkpath(self, file):
     file = os.path.normpath(file)
-    print('creating: ' + file)
     parts = os.path.split(file)
-    os.makedirs(parts[0], exist_ok=True)
+    if len(parts) == 2 and parts[0]:
+      os.makedirs(parts[0], exist_ok=True)
     return file
 
   #
@@ -77,6 +89,10 @@ class CertGenerator:
 
       exts = exts + f' -ext "{extn}={extv}"'  
 
+    # expiration
+    if hasattr(cert, 'expires'):
+      exts = exts + f' -validity {cert.expires}'
+
     return exts
 
   #
@@ -84,8 +100,6 @@ class CertGenerator:
   #
   def load_config(self, config_file, params):  
     
-    print(f'Parameter substitutions: {params}')
-
     with open(config_file) as f:
       p = re.compile('.*\".*{.*}.*\"')
       conf = ""
@@ -101,63 +115,71 @@ class CertGenerator:
       #print(conf)
       #exit(0)
       y = yaml.load(conf)
-      return Munch.fromDict(y)
+      m = Munch.fromDict(y)
+      #print(m)
+      #exit(0)
+      return m
 
 
   def create_cert(self, config, cert, executor):
     """ create certificate based on the specified config """   
     store = config.stores[cert.store]
-
+    
     outfile  = self.__mkpath(store.file)
     keyfile  = self.__mkpath(f'{self.WORK_DIR}/{cert.alias}.key')
     csrfile  = self.__mkpath(f'{self.WORK_DIR}/{cert.alias}.csr')
     crtfile  = self.__mkpath(f'{self.WORK_DIR}/{cert.alias}.crt')
     rootfile = self.__mkpath(f'{self.WORK_DIR}/root.crt')
     
-    # find the signer. If it is not explicitly in config
-    if '.' in cert.issuer:
-      castore = cert.issuer.split('.')[0]
-      castore = config.stores[castore]
-      issuer  = cert.issuer.split('.')[1]
-    else:
-      castore = store
-      issuer  = cert.issuer
-
-    if ':' in issuer:
-      issuerpass = issuer.split(':')[1]
-      issuer     = issuer.split(':')[0]
-    else:
-      issuerpass = issuer.key.password
-
-    cmd0 = f'keytool -exportcert -alias cordarootca -keystore {castore.file} -file {rootfile} -storepass {castore.password} -v'
-    executor(cmd0)
-
     # delete the existing keypair
-    cmd0 = f'keytool -delete -alias {cert.alias} -keystore {outfile} -storepass {castore.password}'
+    cmd0 = f'keytool -delete -alias {cert.alias} -keystore {outfile} -storepass {store.password}'
     executor(cmd0)
 
     # create self-signed keypair ( & cert)
     cmd1 = f'keytool -genkeypair -dname "{cert.subject}" -alias {cert.alias} -keyalg EC -keysize 256 -keystore {outfile} -storepass {store.password} -keypass {cert.key.password} -v'
     cmd1 = cmd1 + self.__extensions_str(cert)
-    executor(cmd1)
+    executor(cmd1)    
 
-    # create CSR for issuer to sign
-    cmd2 = f'keytool -certreq -alias {cert.alias} -file {csrfile} -keystore {outfile} -storepass {store.password} -keypass {cert.key.password} -v'
-    executor(cmd2)
+    if cert.issuer:
+      # find the signer. If it is not explicitly in config
+      if '.' in cert.issuer:
+        castore = cert.issuer.split('.')[0]
+        castore = config.stores[castore]
+        issuer  = cert.issuer.split('.')[1]
+      else:
+        castore = store
+        issuer  = cert.issuer
 
-    # sign the CSR
-    cmd3 = f'keytool -gencert -alias {issuer} -ext honored=all -infile {csrfile} -outfile {crtfile} -keystore {castore.file} -storepass {castore.password} -keypass {issuerpass} -v'
-    cmd3 = cmd3 + self.__extensions_str(cert)
-    executor(cmd3)
+      if ':' in issuer:
+        issuerpass = issuer.split(':')[1]
+        issuer     = issuer.split(':')[0]
+      else:
+        issuerpass = issuer.key.password
 
-    # update with the signed copy
-    cmd4 = f'keytool -importcert -alias {cert.alias} -keystore {outfile} -storepass {store.password} -noprompt -trustcacerts -keypass {cert.key.password} -v'
+      cmd0 = f'keytool -exportcert -alias cordarootca -keystore {castore.file} -file {rootfile} -storepass {castore.password} -v'
+      executor(cmd0)
 
-    cat = 'type' if os.name == 'nt' else 'cat'  
-    cmd4 = f'{cat} {crtfile} {rootfile} | {cmd4}'
-    
-    executor(cmd4)
-  
+      # create CSR for issuer to sign
+      cmd2 = f'keytool -certreq -alias {cert.alias} -file {csrfile} -keystore {outfile} -storepass {store.password} -keypass {cert.key.password} -v'
+      executor(cmd2)
+
+      # sign the CSR
+      cmd3 = f'keytool -gencert -alias {issuer} -ext honored=all -infile {csrfile} -outfile {crtfile} -keystore {castore.file} -storepass {castore.password} -keypass {issuerpass} -v'
+      cmd3 = cmd3 + self.__extensions_str(cert)
+      executor(cmd3)
+
+      # update with the signed copy
+      cmd4 = f'keytool -importcert -alias {cert.alias} -keystore {outfile} -storepass {store.password} -noprompt -trustcacerts -keypass {cert.key.password} -v'
+
+      cat = 'type' if os.name == 'nt' else 'cat'  
+      cmd4 = f'{cat} {crtfile} {rootfile} | {cmd4}'
+      
+      executor(cmd4)
+    else:
+      # self-signed - nothing else to do
+      pass
+
+    print(f' --> {store.file}')
 
   def __apply_store_defaults( store, alias):
     try:    
@@ -171,10 +193,13 @@ class CertGenerator:
     if not hasattr(cert, 'alias'):
       cert.alias = alias
 
+    if not hasattr(cert, 'issuer'):
+      cert.issuer = None      
+
     if not hasattr(cert, 'password'):
       cert.password = 'password'
 
-  def generate(self, config, executor=execute):
+  def generate(self, config, verbose=False, executor=execute):
 
     # preprocess the stores section
     for store in config.stores:
@@ -189,10 +214,14 @@ class CertGenerator:
     # generate the certificates
     for alias in config.certificates:
       cert = config.certificates[alias]                  
-      print(Fore.BLACK + Style.BRIGHT + f'@echo ' + '-'*100)
-      print(Fore.BLACK + Style.BRIGHT + f'@echo generating: {cert.alias} - "{cert.subject}"')
-      print(Fore.BLACK + Style.BRIGHT + f'@echo ' + '-'*100)
-      print(Style.RESET_ALL)
+      if verbose:
+        print(Fore.BLACK + Style.BRIGHT + f'@echo ' + '-'*100)
+        print(Fore.BLACK + Style.BRIGHT + f'@echo generating: {cert.alias} - "{cert.subject}"')
+        print(Fore.BLACK + Style.BRIGHT + f'@echo ' + '-'*100)
+        print(Style.RESET_ALL)
+      else:
+        print(f'generating: {cert.alias} - "{cert.subject}"')
+
       self.create_cert(config, cert, executor)
 
 
@@ -212,6 +241,7 @@ def main(args):
 
   certgen = CertGenerator(args.workdir)
 
+  print(f'Parameter substitutions: {params}')
   cert_config = certgen.load_config(args.config, params)
 
   if args.execute:
@@ -219,7 +249,7 @@ def main(args):
   else:
     executor = print
 
-  certgen.generate(cert_config, executor)
+  certgen.generate(cert_config, True, executor)
 
 
 if __name__ == "__main__":
